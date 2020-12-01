724f17e96c95952421e4f868fd2f3106979f6d8a
Bunmi <fenwadewoyin@yahoo.com>
2020-12-01 02:21:28 -0600

    updates

--- EmployeeDB_Tables.sql
+++ EmployeeDB_Tables.sql
@@ -12,20 +12,12 @@ CREATE TABLE "titles" (
 );
 
 CREATE TABLE "dept_manager" (
-   "dept_no" Varchar   NOT NULL,
-	"emp_no" INT   NOT NULL,
-   CONSTRAINT "pk_dept_manager" PRIMARY KEY (
-        "dept_no","emp_no"
-    )
+    "dept_no" Varchar   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+    "emp_no","dept_no"
+     )
 );
-
---CREATE TABLE dept_manager (
---    dept_no VARCHAR   NOT NULL,
---    emp_no INT   NOT NULL,
---    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
---    FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
---    PRIMARY KEY (dept_no, emp_no)
---);
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
@@ -70,7 +62,6 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
 REFERENCES "departments" ("dept_no");
 
-
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title" FOREIGN KEY("emp_title")
 REFERENCES "titles" ("title_id");
 
@@ -82,3 +73,4 @@ REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
+
